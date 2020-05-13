c8162825935bc0c14ba471b656f1dbb26de494b9
Darshan S R <darshansr.niit@gmail.com>
2020-05-13 14:42:54 +0200

    adding and removing zipcode states to reducers

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,14 +6,16 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { ZipcodeActions } from '../actions/zipcode.actions';
+import { ZipcodeState, zipcodeReducer } from './zip-codes.reducer';
 
 
 export interface State {
-
+  zipcodes:ZipcodeState
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  zipcodes:zipcodeReducer
 };
 
 
--- src/app/reducers/zip-codes.reducer.ts
+++ src/app/reducers/zip-codes.reducer.ts
@@ -1,19 +1,23 @@
 import { Action, createReducer, on } from '@ngrx/store';
+import { ZipcodeActionTypes, ZipcodeActions } from '../actions/zipcode.actions';
 
 
 export const zipCodesFeatureKey = 'zipCodes';
 
-export interface State {
-  zipcode:Array<String>
+export interface ZipcodeState {
+  zipcodes: Array<String>
 }
 
-export const initialState: State = {
-zipcode:[]
+export const initialState: ZipcodeState = {
+  zipcodes: []
 };
 
-
-export const reducer = createReducer(
-  initialState,
-
-);
-
+export function zipcodeReducer(state = initialState, action: ZipcodeActions): ZipcodeState {
+  switch (action.type) {
+    case ZipcodeActionTypes.AddZipcode:
+      return { ...state, zipcodes: [...state.zipcodes, action.zipcode] }
+    case ZipcodeActionTypes.RemoveZipcode:
+      return { ...state, zipcodes: state.zipcodes.filter(item => item !== action.zipcode) }
+  }
+  return state;
+}
